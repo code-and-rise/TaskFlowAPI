50d28cf483d698febb52d6b00e7cd52b89929462
filipmiskovic <filipmiskovic14@yahoo.com>
2026-06-15 19:09:20 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,9 +3,23 @@ const {
 } = require('sequelize');
 
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-});
+
+if (process.env.NODE_ENV === "production") {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    });
+} else {
+    var sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+    });
+
+}
 
 module.exports = sequelize;
